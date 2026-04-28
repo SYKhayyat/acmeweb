49f3f25d08fb5c0ced389a7d3b0e8b6eacc7ef06
SYKhayyat <shaulyoelkhayyat@gmail.com>
2026-04-28 12:10:22 -0400

    Story #3 - made a single class to call all system and runtime calls. This facade is not yet implemented in decorators.

---
+++ src/main/java/com/acme/statusmgr/facade/realSystemInfo.java
@@ -0,0 +1,23 @@
+package com.acme.statusmgr.facade;
+
+/**
+ * This class is the single class which can be called in order to find any system value.
+ * It acts as a facade and hides calls to Runtime and System.
+ */
+public class realSystemInfo {
+    int getAvailableProcessors(){
+        return Runtime.getRuntime().availableProcessors();
+    }
+    long getFreeJVMMemory(){
+        return Runtime.getRuntime().freeMemory();
+    }
+    long getTotalJVMMemory(){
+        return Runtime.getRuntime().totalMemory();
+    }
+    String getJREVersion(){
+        return System.getProperty("java.version");
+    }
+    String getTempLocation(){
+        return System.getenv("TEMP");
+    }
+}
